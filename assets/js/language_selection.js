4badbd53e4ccac534d88d9f311c6d25a7b847bf8
FennecFoxSW <FennecFoxSW@protonmail.com>
2021-08-15 19:00:35 +0900

    Add language selection

---
+++ assets/js/language_selection.js
@@ -0,0 +1,11 @@
+const userLanguage = navigator.language;
+const koreanButton = document.getElementById('ko-button');
+const englishButton = document.getElementById('en-button');
+
+if (userLanguage === 'ko-KR') {
+  englishButton.classList.add('btn-secondary');
+  koreanButton.classList.add('btn-primary');
+} else {
+  koreanButton.classList.add('btn-secondary');
+  englishButton.classList.add('btn-primary');
+}
